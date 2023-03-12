0eea77908621e05705a39b81d3a2193449bd88b4
Md. Golam Hossain <105764760+ghossain004@users.noreply.github.com>
2023-03-12 13:19:52 +0600

    TSP 12.3.23

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,13 @@ class MyHome extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       darkTheme: ThemeData(primarySwatch: Colors.cyan),
       debugShowCheckedModeBanner: false,
-      home: MyApp(),
+      home: MyApp3(),
     );
   }
 }
 
-class MyApp2 extends StatelessWidget {
-  const MyApp2({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   // MySnackBar(message, contex) {
   //   return ScaffoldMessenger.of(contex)
@@ -30,82 +30,226 @@ class MyApp2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Flutter App'),
-        titleSpacing: 0, //default spacing 0
-        centerTitle: true,
-        toolbarHeight: 60, //default height 60
-        toolbarOpacity: 1, //default opacity 1
-        backgroundColor: Colors.deepPurple,
-        actions: [
-          IconButton(
-              onPressed: () {
-                print("menu clicked");
-                DropdownMenuItem;
-              },
-              icon: Icon(Icons.menu))
-        ],
-      ),
-      body: Container(
-        child: Text('Hello World'),
-      ),
-      drawer: Container(
-        child: Container(
-          child: Text('child drawer activated'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Flutter App'),
+          titleSpacing: 0, //default spacing 0
+          centerTitle: true,
+          toolbarHeight: 60, //default height 60
+          toolbarOpacity: 1, //default opacity 1
+          backgroundColor: Colors.deepPurple,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  print("menu clicked");
+                  DropdownMenuItem;
+                },
+                icon: Icon(Icons.menu))
+          ],
         ),
-      ),
-      endDrawer: Container(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // print('button print');
-          MySnackBar('hi!', context);
-        },
-        child: Icon(Icons.arrow_upward),
-        backgroundColor: Colors.deepPurple,
+        body: Container(
+          child: Text('Hello World'),
+        ),
+        drawer: Container(
+          child: Container(
+            child: Text('child drawer activated'),
+          ),
+        ),
+        endDrawer: Container(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // print('button print');
+            MySnackBar('hi!', context);
+          },
+          child: Icon(Icons.arrow_upward),
+          backgroundColor: Colors.deepPurple,
+        ),
       ),
     );
   }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
       darkTheme: ThemeData(primarySwatch: Colors.cyan),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
+        body: ListView(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.greenAccent,
+            ),
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.blueAccent,
+            ),
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.redAccent,
+            ),
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.purpleAccent,
+            ),
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.orangeAccent,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp3 extends StatelessWidget {
+  const MyApp3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Row(
             children: [
-              Container(
-                height: 200.0,
-                width: double.infinity,
-                color: Colors.greenAccent,
+              // flex: 2,
+              Expanded(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width / 6,
+                  color: Colors.blueGrey,
+                ),
               ),
-              Container(
-                height: 200.0,
-                width: double.infinity,
-                color: Colors.blueAccent,
+
+              Expanded(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width / 6,
+                  color: Colors.blue,
+                ),
               ),
-              Container(
-                height: 200.0,
-                width: double.infinity,
-                color: Colors.redAccent,
+              Expanded(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width / 6,
+                  color: Colors.white,
+                ),
               ),
-              Container(
-                height: 200.0,
-                width: double.infinity,
-                color: Colors.purpleAccent,
+              Expanded(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width / 6,
+                  color: Colors.deepOrange,
+                ),
               ),
-              Container(
-                height: 200.0,
-                width: double.infinity,
-                color: Colors.orangeAccent,
+              Expanded(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width / 6,
+                  color: Colors.green,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width / 6,
+                  color: Colors.indigo,
+                ),
               )
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp4 extends StatelessWidget {
+  const MyApp4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height / 5,
+              width: MediaQuery.of(context).size.width / 3,
+              color: Colors.lightBlue,
+              child: const Center(
+                child: Text('Hello World'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp5 extends StatelessWidget {
+  const MyApp5({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Stack(
+              alignment: Alignment.topLeft,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.green,
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height / 3,
+                  left: MediaQuery.of(context).size.width / 4,
+                  child: CircleAvatar(
+                    radius: 100,
+                    backgroundColor: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp6 extends StatelessWidget {
+  const MyApp6({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            // child: Image.asset('images/robot.jpg'),
+            child: Image.network(
+                'https://e1.pxfuel.com/desktop-wallpaper/272/895/desktop-wallpaper-3d-for-android-mobile-3d-mobile-thumbnail.jpg'),
           ),
         ),
       ),
